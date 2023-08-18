6f9d1d86ea4df9e7c1c83fbac5f309b793aa4426
Shoaib Akhtar <shoaibakhtar9430@gmail.com>
2023-08-18 11:49:28 +0530

    first commit

---
+++ server/config/dbConfig.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.mongo_url);
+
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+    console.log('Mongo db connected successfully');
+})
+
+connection.on('error', (err) => {
+    console.log('Mongo db connection error: ', err);
+})
+
+module.exports = mongoose;
+
+// mongodb: //127.0.0.1:27017/TaskManagmentApp
